c8f795c3c89506286a21b16cdfb106599b7e3305
ilmong06 <ilmongcoding@gmail.com>
2024-03-22 18:20:29 +0900

    3/22 중간보고

--- src/main/java/org/example/controller/ArticleController.java
+++ src/main/java/org/example/controller/ArticleController.java
@@ -40,7 +40,7 @@ public class ArticleController extends Controller{
                 break;
             default:
                 System.out.println("존재하지 않는 명령어 입니다.");
-                 break;
+                break;
         }
 
 
--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -31,12 +31,26 @@ public class MemberController extends Controller {
             case "login":
                 doLogin();
                 break;
+            case "logout":
+                doLogout();
+                break;
             default:
                 System.out.println("존재하지 않는 명령어 입니다.");
                 break;
         }
 
     }
+
+
+
+    public void makeTestData() {
+        System.out.println("테스트를 위한 게시물 데이터를 생성합니다");
+
+        members.add(new Member(1, Util.getNowDateStr(), "admin", "admin", "관리자"));
+        members.add(new Member(2, Util.getNowDateStr(), "user1","user1", "홍길동"));
+        members.add(new Member(3, Util.getNowDateStr(), "user2", "user2", "홍길순"));
+    }
+
 
     public void doJoin() {
         int id = members.size() + 1;
@@ -83,6 +97,11 @@ public class MemberController extends Controller {
     }
 
     public void doLogin() {
+        if(isLogined()){
+            System.out.println("이미 로그인 되어 있습니다");
+            return;
+        }
+
         System.out.printf("로그인 아이디 : ");
         String loginId = sc.nextLine();
         System.out.printf("로그인 비번 : ");
@@ -135,5 +154,22 @@ public class MemberController extends Controller {
         }
 
         return members.get(index);
+
+
+    }
+
+    private boolean isLogined(){
+        return  loginedMember != null;
+    }
+
+    private void doLogout(){
+        if( isLogined() == false){
+            System.out.println("로그인 상태가 아닙니다");
+            return;
+        }
+        loginedMember = null;
+        System.out.println("로그아웃 되었습니다.");
     }
 }
+
+
